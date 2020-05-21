7522f61de859204650f6af351f67db80140fddd2
AnaOzuna01 <20175076@ce.pucmm.edu.do>
2020-05-20 23:21:25 -0400

    Arreglando errores en archivos js.

--- js/Cirsulo.js
+++ js/Cirsulo.js
@@ -2,5 +2,4 @@ var PI = 3.1415926535;
 var radio = prompt('Introduzca el radio del círculo:', '');
 var area;
 area = PI * radio * radio;
-document.write("<header><h1>El área del círculo es: " + area + "</h1><hr /
-        > < br / > < /header>");
+document.write("<header><h1>El área del círculo es: " + area + "</h1><hr/> <br/></header>");
