c8047c175444e4a7d3d561b62aa44cc62b228e8e
AnaOzuna01 <20175076@ce.pucmm.edu.do>
2020-05-20 23:19:19 -0400

    Agregando archivos js.

---
+++ js/Rectangulo.js
@@ -0,0 +1,6 @@
+var base = prompt('Introduzca la base del rectángulo', '');
+var altura = prompt('Introduzca la altura del rectángulo', '');
+var area;
+area = base * altura
+document.write("<header><h1>El área del rectángulo es: " + area +
+    "</h1><hr /><br /></header>");
